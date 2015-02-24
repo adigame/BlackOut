30283718a4cb8411d026718b27d9e28bc680906b
Wawixs <wawiixs@gmail.com>
2015-02-24 21:44:23 +0100

    bat

--- BlackOut v3 [Refonte]/Launcher Altis Life.bat
+++ BlackOut v3 [Refonte]/Launcher Altis Life.bat
@@ -8,7 +8,7 @@ timeout 1
 
 cd C:\Program Files (x86)\Steam\steamapps\common\Arma 3
 timeout 3
-start "arma3" /min /high "C:\Program Files (x86)\Steam\steamapps\common\Arma 3\arma3server.exe" -mod=@life_server;@extDB;@BlackOut -noPause -noSound -name=DefaultServer -profiles=config -config=config\server.cfg -cfg=config\basic.cfg -port=2302 -ip=192.168.0.34
+start "arma3" /min /high "C:\Program Files (x86)\Steam\steamapps\common\Arma 3\arma3server.exe" -mod=@life_server;@extDB;@BlackOut -noPause -noSound -name=DefaultServer -profiles=config -config=config\server.cfg -cfg=config\basic.cfg -port=2302 -ip=192.168.1.28
 timeout 6
 echo ARMA 3 Server se lance
-exit 
+exit
