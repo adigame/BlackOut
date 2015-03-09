99dce75b25de19bff1a2ce078728e1e43fc63333
Wawixs <wawiixs@gmail.com>
2015-03-09 14:04:12 +0100

    DB

--- Niktamé/default.sql
+++ Niktamé/default.sql
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `players` (
   `med_licenses` text,
   `cop_gear` text NOT NULL,
   `med_gear` text NOT NULL,
-  `mediclevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
+  `mediclevel` enum('0','1','2','3','4','5','6') NOT NULL DEFAULT '0',
   `arrested` tinyint(1) NOT NULL DEFAULT '0',
   `aliases` text NOT NULL,
   `adminlevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
